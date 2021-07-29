1f01395cd237b21133dbea6c23a6d3ae6f92efb1
littleOneYuan <53067129+littleOneYuan@users.noreply.github.com>
2021-07-29 11:11:42 +0800

    Create time.js

---
+++ data/time.js
@@ -0,0 +1,193 @@
+export const time_arr = [{
+    text: '00:30',
+    half: true,
+    focus: false
+}, {
+    text: '01:00',
+    half: false,
+    focus: false
+}, {
+    text: '01:30',
+    half: true,
+    focus: false
+}, {
+    text: '02:00',
+    half: false,
+    focus: false
+}, {
+    text: '02:30',
+    half: true,
+    focus: false
+}, {
+    text: '03:00',
+    half: false,
+    focus: false
+}, {
+    text: '03:30',
+    half: true,
+    focus: false
+}, {
+    text: '04:00',
+    half: false,
+    focus: false
+}, {
+    text: '04:30',
+    half: true,
+    focus: false
+}, {
+    text: '05:00',
+    half: false,
+    focus: false
+}, {
+    text: '05:30',
+    half: true,
+    focus: false
+}, {
+    text: '06:00',
+    half: false,
+    focus: false
+}, {
+    text: '06:30',
+    half: true,
+    focus: false
+}, {
+    text: '07:00',
+    half: false,
+    focus: false
+}, {
+    text: '07:30',
+    half: true,
+    focus: false
+}, {
+    text: '08:00',
+    half: false,
+    focus: false
+}, {
+    text: '08:30',
+    half: true,
+    focus: false
+}, {
+    text: '09:00',
+    half: false,
+    focus: false
+}, {
+    text: '09:30',
+    half: true,
+    focus: false
+}, {
+    text: '10:00',
+    half: false,
+    focus: false
+}, {
+    text: '10:30',
+    half: true,
+    focus: false
+}, {
+    text: '11:00',
+    half: false,
+    focus: false
+}, {
+    text: '11:30',
+    half: true,
+    focus: false
+}, {
+    text: '12:00',
+    half: false,
+    focus: true
+}, {
+    text: '12:30',
+    half: true,
+    focus: false
+}, {
+    text: '13:00',
+    half: false,
+    focus: false
+}, {
+    text: '13:30',
+    half: true,
+    focus: false
+}, {
+    text: '14:00',
+    half: false,
+    focus: false
+}, {
+    text: '14:30',
+    half: true,
+    focus: false
+}, {
+    text: '15:00',
+    half: false,
+    focus: false
+}, {
+    text: '15:30',
+    half: true,
+    focus: false
+}, {
+    text: '16:00',
+    half: false,
+    focus: false
+}, {
+    text: '16:30',
+    half: true,
+    focus: false
+}, {
+    text: '17:00',
+    half: false,
+    focus: false
+}, {
+    text: '17:30',
+    half: true,
+    focus: false
+}, {
+    text: '18:00',
+    half: false,
+    focus: false
+}, {
+    text: '18:30',
+    half: true,
+    focus: false
+}, {
+    text: '19:00',
+    half: false,
+    focus: false
+}, {
+    text: '19:30',
+    half: true,
+    focus: false
+}, {
+    text: '20:00',
+    half: false,
+    focus: false
+}, {
+    text: '20:30',
+    half: true,
+    focus: false
+}, {
+    text: '21:00',
+    half: false,
+    focus: false
+}, {
+    text: '21:30',
+    half: true,
+    focus: false
+}, {
+    text: '22:00',
+    half: false,
+    focus: false
+}, {
+    text: '22:30',
+    half: true,
+    focus: false
+}, {
+    text: '23:00',
+    half: false,
+    focus: false
+}, {
+    text: '23:30',
+    half: true,
+    focus: false
+}, {
+    text: '24:00',
+    half: false,
+    focus: false
+}]
